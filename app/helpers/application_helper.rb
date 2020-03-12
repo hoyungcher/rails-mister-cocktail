e4cab63cad87c6268b1cf27df5d12af1c2b5f916
Yung Cher Ho <ycho527@hotmail.com>
2020-03-12 16:43:06 +0800

    Completed cocktail app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  COCKTAIL_COLOURS = { yellow: "#FFC65A",
+                       red: "#B61F34",
+                       orange: "#E0512B",
+                       green: "#009551",
+                       blue: "#46D0E6" }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+
+ingredients = []
+user["drinks"].each do |ingredient|
+  ingredients << ingredient["strIngredient1"]
+end
+
+ingredients.each do |ingredient|
+  Ingredient.create(name: ingredient)
+end
